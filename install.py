907f65796d1c30bad57f7014cfc5e3d3051b4e85
scragly <29337040+scragly@users.noreply.github.com>
2017-10-19 07:52:24 +1000

    Create install.py

---
+++ install.py
@@ -0,0 +1,38 @@
+import subprocess
+import pip
+from sys import platform
+
+_apt_req_= [
+    "tesseract-ocr",
+    "tesseract-ocr-eng"
+]
+
+apt_cmd = "apt install "
+
+_pip_req_= [
+    "discord.py>=0.16.6",
+    "pillow>=4.2.1",
+    "requests>=2.18.4",
+    "pytesseract>=0.1.7",
+    "hastebin.py>=0.2"
+]
+
+def apt_install(packages):
+    for package in packages:
+        cmd = apt_cmd+package
+        subprocess.run(cmd.split())
+        print("[+] Package {} Installed".format(str(package)))
+
+def pip_install(packages):
+    for package in packages:
+        pip.main(['install',package])
+
+if __name__ == '__main__':
+    if platform.startswith == 'linux':
+        apt_install(_apt_req_)
+    pip_install(_pip_req_)
+    if platform == 'windows':
+        print("Please install Tesseract OCR with the installer for Windows by visiting:\nhttps://github.com/tesseract-ocr/tesseract/wiki/Downloads")
+    if platform == 'darwin':
+        print("Python packages installed. Please ensure Tesseract OCR for OSX is installed manually.")
+    print("Requirements installation is now complete.")

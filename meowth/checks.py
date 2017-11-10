c64f526f9bde8b2ac9bd03eca24826ea7ce7c601
BrenenP <xdooncex@gmail.com>
2017-11-10 17:15:06 -0500

    Start exraid timerset

--- meowth/checks.py
+++ meowth/checks.py
@@ -99,10 +99,11 @@ def check_exraidchannel(ctx):
     channel = ctx.message.channel
     server = ctx.message.server
     try:
-        type = ctx.bot.server_dict[server]['raidchannel_dict'][channel]['egglevel']
+        level = ctx.bot.server_dict[server]['raidchannel_dict'][channel]['egglevel']
+        type = ctx.bot.server_dict[server]['raidchannel_dict'][channel]['type']
     except KeyError:
         return False
-    if type == 'EX':
+    if level == 'EX' or type == 'exraid':
         return True
 
 def check_raidactive(ctx):

fc917e9a63ba3aca6d1d47859f88d5ebf795f941
BrenenP <xdooncex@gmail.com>
2018-02-01 16:37:04 -0500

    !setstatus fix

--- meowth/__main__.py
+++ meowth/__main__.py
@@ -1354,11 +1354,10 @@ async def setstatus(ctx, user, status, count=None):
             count = server_dict[ctx.message.server.id]['raidchannel_dict'][ctx.message.channel.id]['trainer_dict'][user.id]['count']
         except KeyError:
             count = 1
-    if not party:
-        try:
-            party = server_dict[ctx.message.server.id]['raidchannel_dict'][ctx.message.channel.id]['trainer_dict'][user.id]['party']
-        except KeyError:
-            party = [0,0,0,count]
+    try:
+        party = server_dict[ctx.message.server.id]['raidchannel_dict'][ctx.message.channel.id]['trainer_dict'][user.id]['party']
+    except KeyError:
+        party = [0,0,0,count]
     if status == "maybe" or status == "interested" or status == "i":
         await _maybe(ctx.message.channel, user, count, party)
     elif status == "omw" or status == "coming" or status == "c":

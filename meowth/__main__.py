6946c5b61a5c09d37fabe277b746f65ba7c9b9c3
FoglyOgly <lexyk.xiv@gmail.com>
2018-04-02 14:12:40 -0500

    bugfix

--- meowth/__main__.py
+++ meowth/__main__.py
@@ -30,6 +30,10 @@ import hastebin
 from operator import itemgetter
 from errors import custom_error_handling
 import dateparser
+import io
+import textwrap
+import traceback
+from contextlib import redirect_stdout
 tessdata_dir_config = "--tessdata-dir 'C:\\Program Files (x86)\\Tesseract-OCR\\tessdata' "
 xtraconfig = '-l eng -c tessedit_char_blacklist=&|=+%#^*[]{};<> -psm 6'
 if os.name == 'nt':
@@ -1000,6 +1004,57 @@ async def on_message_delete(message):
 """
 Admin Commands
 """
+
+@Meowth.command(hidden=True, name="eval")
+@Meowth.is_owner()
+async def _eval(ctx, *, body: str):
+    """Evaluates a code"""
+    env = {
+        'bot': ctx.bot,
+        'ctx': ctx,
+        'channel': ctx.channel,
+        'author': ctx.author,
+        'guild': ctx.guild,
+        'message': ctx.message
+    }
+    def cleanup_code(content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+        # remove `foo`
+        return content.strip('` \n')
+    env.update(globals())
+    body = cleanup_code(body)
+    stdout = io.StringIO()
+    to_compile = (f'async def func():\n{textwrap.indent(body, "  ")}')
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+    else:
+        value = stdout.getvalue()
+        try:
+            await ctx.message.add_reaction('\u2705')
+        except:
+            pass
+        if ret is None:
+            if value:
+                paginator = commands.Paginator(prefix='```py')
+                for line in textwrap.wrap(value, 80):
+                    paginator.add_line(line.rstrip().replace('`', '\u200b`'))
+                for p in paginator.pages:
+                    await ctx.send(p)
+        else:
+            ctx.bot._last_result = ret
+            await ctx.send(f'```py\n{value}{ret}\n```')
 
 @Meowth.command()
 @checks.is_owner()

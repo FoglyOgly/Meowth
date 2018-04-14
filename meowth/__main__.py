fa29ad2e8e651bca67da5676c3628d46bd832e25
BrenenP <xdooncex@gmail.com>
2018-04-14 10:05:05 -0400

    indent error

--- meowth/__main__.py
+++ meowth/__main__.py
@@ -804,7 +804,7 @@ async def message_cleanup(loop=True):
                                 report_delete_dict[reportid] = {"action":report_dict_dict[report_dict][reportid]['expedit'],"channel":report_channel}
                             else:
                                 report_edit_dict[reportid] = {"action":report_dict_dict[report_dict][reportid]['expedit'],"channel":report_channel}
-                    del guild_dict[guildid][report_dict][reportid]
+                        del guild_dict[guildid][report_dict][reportid]
             for messageid in report_delete_dict.keys():
                 try:
                     report_message = await report_delete_dict[messageid]['channel'].get_message(messageid)

836baea8e15a483bc49c937c28839fd3ddd7f69b
Makonde <makonde.fasc@gmail.com>
2018-07-12 22:12:10 +0200

    add gymmathing to default extensions

--- meowth/__main__.py
+++ meowth/__main__.py
@@ -133,7 +133,7 @@ Meowth.config = config
 Meowth.pkmn_info_path = pkmn_path
 Meowth.raid_json_path = raid_path
 
-default_exts = ['datahandler', 'tutorial', 'silph', 'utilities']
+default_exts = ['datahandler', 'tutorial', 'silph', 'utilities', 'gymmatching']
 
 for ext in default_exts:
     try:

d5e8fe2e4a9fb41552a491aca2f9a9937c4622ae
FoglyOgly <lexyk.xiv@gmail.com>
2018-08-01 14:02:20 -0500

    color names

--- meowth/exts/teams/tables.py
+++ meowth/exts/teams/tables.py
@@ -193,8 +193,112 @@ def setup(bot):
             "color_id": 3,
             "language_id": 2,
             "name": "red"
+        },
+        {
+            "color_id": 1,
+            "language_id": 3,
+            "name": "あお"
+        },
+        {
+            "color_id": 1,
+            "language_id": 4,
+            "name": "青"
+        },
+        {
+            "color_id": 1,
+            "language_id": 5, 
+            "name": "ao"
+        },
+        {
+            "color_id": 2,
+            "language_id": 3,
+            "name": "きいろ"
+        },
+        {
+            "color_id": 2,
+            "language_id": 4,
+            "name": "黄色"
+        },
+        {
+            "color_id": 2,
+            "language_id": 5,
+            "name": "ki iro"
+        },
+        {
+            "color_id": 3,
+            "language_id": 3,
+            "name": "あか"
+        },
+        {
+            "color_id": 3,
+            "language_id": 4,
+            "name": "赤"
+        },
+        {
+            "color_id": 3,
+            "language_id": 5,
+            "name": "aka"
+        },
+        {
+            "color_id": 1,
+            "language_id": 8,
+            "name": "bleu"
+        },
+        {
+            "color_id": 2,
+            "language_id": 8,
+            "name": "jaune"
+        },
+        {
+            "color_id": 3,
+            "language_id": 8,
+            "name": "rouge"
+        },
+        {
+            "color_id": 1,
+            "language_id": 9,
+            "name": "blau"
+        },
+        {
+            "color_id": 2,
+            "language_id": 9,
+            "name": "gelb"
+        },
+        {
+            "color_id": 3,
+            "language_id": 9,
+            "name": "rot"
+        },
+        {
+            "color_id": 1,
+            "language_id": 10,
+            "name": "azul"
+        },
+        {
+            "color_id": 2,
+            "language_id": 10,
+            "name": "amarillo"
+        },
+        {
+            "color_id": 3,
+            "language_id": 10,
+            "name": "rojo"
+        },
+        {
+            "color_id": 1,
+            "language_id": 11,
+            "name": "azzurro"
+        },
+        {
+            "color_id": 2,
+            "language_id": 11,
+            "name": "giallo"
+        },
+        {
+            "color_id": 3,
+            "language_id": 11,
+            "name": "rosso"
         }
-        # non-English names missing
     ]
 
 

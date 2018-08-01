fb9f55a508c58c2bead4a3ade93bba24172af46a
FoglyOgly <lexyk.xiv@gmail.com>
2018-08-01 15:06:11 -0500

    fix language ids

--- meowth/exts/i18n/tables.py
+++ meowth/exts/i18n/tables.py
@@ -12,84 +12,84 @@ def setup(bot):
 
     lang_table.initial_data = [
         {
-            "language_id": 1,
+            "language_id": 9,
             "iso639": "en",
             "iso3166": "us",
             "identifier": "en",
             "official": True
         },
         {
-            "language_id": 2,
+            "language_id": 12,
             "iso639": "en",
             "iso3166": "gb",
             "identifier": "en-gb",
             "official": False
         },
         {
-            "language_id": 3,
+            "language_id": 1,
             "iso639": "ja",
             "iso3166": "jp",
             "identifier": "ja",
             "official": True
         },
         {
-            "language_id": 4,
+            "language_id": 11,
             "iso639": "ja",
             "iso3166": "jp",
             "identifier": "ja-kanji",
             "official": True
         },
         {
-            "language_id": 5,
+            "language_id": 2,
             "iso639": "ja",
             "iso3166": "jp",
             "identifier": "roomaji",
             "official": True
         },
         {
-            "language_id": 6,
+            "language_id": 3,
             "iso639": "ko",
             "iso3166": "kr",
             "identifier": "ko",
             "official": True
         },
         {
-            "language_id": 7,
+            "language_id": 4,
             "iso639": "zh",
             "iso3166": "cn",
             "identifier": "zh",
             "official": True
         },
         {
-            "language_id": 8,
+            "language_id": 5,
             "iso639": "fr",
             "iso3166": "fr",
             "identifier": "fr",
             "official": True
         },
         {
-            "language_id": 9,
+            "language_id": 6,
             "iso639": "de",
             "iso3166": "de",
             "identifier": "de",
             "official": True
         },
         {
-            "language_id": 10,
+            "language_id": 7,
             "iso639": "es",
             "iso3166": "es",
             "identifier": "es",
             "official": True
         },
         {
-            "language_id": 11,
+            "language_id": 8,
             "iso639": "it",
             "iso3166": "it",
             "identifier": "it",
             "official": True
         },
         {
-            "language_id": 12,
+            "language_id": 10,
             "iso639": "cs",
             "iso3166": "cz",
             "identifier": "cs",
--- meowth/exts/teams/tables.py
+++ meowth/exts/teams/tables.py
@@ -43,122 +43,122 @@ def setup(bot):
     team_names_table.initial_data = [
         {
             "team_id": 1,
+            "language_id": 9,
+            "name": "mystic"
+        },
+        {
+            "team_id": 1,
+            "language_id": 12,
+            "name": "mystic"
+        },
+        {
+            "team_id": 1,
             "language_id": 1,
-            "name": "mystic"
-        },
-        {
-            "team_id": 1,
-            "language_id": 2,
-            "name": "mystic"
-        },
-        {
-            "team_id": 1,
-            "language_id": 3,
             "name": "ミスティック"
         },
         {
             "team_id": 1,
-            "language_id": 5,
+            "language_id": 2,
             "name": "misutikku"
         },
         {
             "team_id": 1,
-            "language_id": 8,
+            "language_id": 5,
             "name": "sagesse" 
         },
         {
             "team_id": 1,
-            "language_id": 9,
+            "language_id": 6,
             "name": "weisheit"
         },
         {
             "team_id": 1,
-            "language_id": 10,
+            "language_id": 7,
             "name": "sabiduría"
         },
         {
             "team_id": 1,
-            "language_id": 11,
+            "language_id": 8,
             "name": "saggezza"
         },
         {
             "team_id": 2,
+            "language_id": 9,
+            "name": "instinct"
+        },
+        {
+            "team_id": 2,
+            "language_id": 12,
+            "name": "instinct"
+        },
+        {
+            "team_id": 2,
             "language_id": 1,
-            "name": "instinct"
-        },
-        {
-            "team_id": 2,
-            "language_id": 2,
-            "name": "instinct"
-        },
-        {
-            "team_id": 2,
-            "language_id": 3,
             "name": "インスティンクト"
         },
         {
             "team_id": 2,
-            "language_id": 5,
+            "language_id": 2,
             "name": "insutinkuto"
         },
         {
             "team_id": 2,
-            "language_id": 8,
+            "language_id": 5,
             "name": "intuition"
         },
         {
             "team_id": 2,
-            "language_id": 9,
+            "language_id": 6,
             "name": "intuition"
         },
         {
             "team_id": 2,
-            "language_id": 10,
+            "language_id": 7,
             "name": "instinto"
         },
         {
             "team_id": 2,
-            "language_id": 11,
+            "language_id": 8,
             "name": "istinto"
         },
         {
             "team_id": 3,
-            "language_id": 1,
+            "language_id": 9,
             "name": "valor"
         },
         {
             "team_id": 3,
-            "language_id": 2,
+            "language_id": 12,
             "name": "valour"
         },
         {
             "team_id": 3,
-            "language_id": 3,
+            "language_id": 1,
             "name": "ヴァーラー"
         },
         {
             "team_id": 3,
-            "language_id": 5,
+            "language_id": 2,
             "name": "ba-ra"
         },
         {
             "team_id": 3,
-            "language_id": 8,
+            "language_id": 5,
             "name": "bravoure"
         },
         {
             "team_id": 3,
-            "language_id": 9,
+            "language_id": 6,
             "name": "wagemut"
         },
         {
             "team_id": 3,
-            "language_id": 10,
+            "language_id": 7,
             "name": "valor"
         },
         {
             "team_id": 3,
-            "language_id": 11,
+            "language_id": 8,
             "name": "coraggio"
         }
     ]
@@ -166,137 +166,137 @@ def setup(bot):
     color_names_table.initial_data = [
         {
             "color_id": 1,
-            "language_id": 1,
+            "language_id": 9,
             "name": "blue"
         },
         {
             "color_id": 2,
-            "language_id": 1,
+            "language_id": 9,
             "name": "yellow"
         },
         {
             "color_id": 3,
-            "language_id": 1,
+            "language_id": 9,
             "name": "red"
         },
         {
             "color_id": 1,
-            "language_id": 2,
+            "language_id": 12,
             "name": "blue"
         },
         {
             "color_id": 2,
-            "language_id": 2,
+            "language_id": 12,
             "name": "yellow"
         },
         {
             "color_id": 3,
-            "language_id": 2,
+            "language_id": 12,
             "name": "red"
         },
         {
             "color_id": 1,
-            "language_id": 3,
+            "language_id": 1,
             "name": "あお"
         },
         {
             "color_id": 1,
-            "language_id": 4,
+            "language_id": 11,
             "name": "青"
         },
         {
             "color_id": 1,
-            "language_id": 5, 
+            "language_id": 2, 
             "name": "ao"
         },
         {
             "color_id": 2,
-            "language_id": 3,
+            "language_id": 1,
             "name": "きいろ"
         },
         {
             "color_id": 2,
-            "language_id": 4,
+            "language_id": 11,
             "name": "黄色"
         },
         {
             "color_id": 2,
-            "language_id": 5,
+            "language_id": 2,
             "name": "ki iro"
         },
         {
             "color_id": 3,
-            "language_id": 3,
+            "language_id": 1,
             "name": "あか"
         },
         {
             "color_id": 3,
-            "language_id": 4,
+            "language_id": 11,
             "name": "赤"
         },
         {
             "color_id": 3,
-            "language_id": 5,
+            "language_id": 2,
             "name": "aka"
         },
         {
             "color_id": 1,
-            "language_id": 8,
+            "language_id": 5,
             "name": "bleu"
         },
         {
             "color_id": 2,
-            "language_id": 8,
+            "language_id": 5,
             "name": "jaune"
         },
         {
             "color_id": 3,
-            "language_id": 8,
+            "language_id": 5,
             "name": "rouge"
         },
         {
             "color_id": 1,
-            "language_id": 9,
+            "language_id": 6,
             "name": "blau"
         },
         {
             "color_id": 2,
-            "language_id": 9,
+            "language_id": 6,
             "name": "gelb"
         },
         {
             "color_id": 3,
-            "language_id": 9,
+            "language_id": 6,
             "name": "rot"
         },
         {
             "color_id": 1,
-            "language_id": 10,
+            "language_id": 7,
             "name": "azul"
         },
         {
             "color_id": 2,
-            "language_id": 10,
+            "language_id": 7,
             "name": "amarillo"
         },
         {
             "color_id": 3,
-            "language_id": 10,
+            "language_id": 7,
             "name": "rojo"
         },
         {
             "color_id": 1,
-            "language_id": 11,
+            "language_id": 8,
             "name": "azzurro"
         },
         {
             "color_id": 2,
-            "language_id": 11,
+            "language_id": 8,
             "name": "giallo"
         },
         {
             "color_id": 3,
-            "language_id": 11,
+            "language_id": 8,
             "name": "rosso"
         }
     ]

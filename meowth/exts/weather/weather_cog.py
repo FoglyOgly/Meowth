7f962c836976f2456c36f735fdff2d6a88888e2e
FoglyOgly <kogle@pearidgek12.com>
2018-11-09 15:07:53 -0600

    debug

--- meowth/exts/weather/weather_cog.py
+++ meowth/exts/weather/weather_cog.py
@@ -65,6 +65,7 @@ class WeatherCog(Cog):
                         update[str(time)] = weather.value
             weather_update.where(cellid=cell)
             weather_update.values(**update)
+            print(weather_update.sql)
             await weather_update.commit()
         
 

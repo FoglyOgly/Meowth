cd80a3972fb6cef4a9f23871fc9ce12c5377d64b
FoglyOgly <kogle@pearidgek12.com>
2018-11-14 07:15:55 -0600

    fix import

--- meowth/exts/pkmn/__init__.py
+++ meowth/exts/pkmn/__init__.py
@@ -1,6 +1,6 @@
 """This cog contains features relating to Pokemon."""
 
-from .pkmn_cog import Pokedex, Pokemon, Move, RaidBoss
+from .pkmn_cog import Pokedex, Pokemon, Move
 
 def setup(bot):
     bot.add_cog(Pokedex(bot))

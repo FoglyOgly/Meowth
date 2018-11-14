cc6aab73898fdb371ed41651184fe8e7a0fba097
FoglyOgly <kogle@pearidgek12.com>
2018-11-14 07:48:53 -0600

    int to str

--- meowth/exts/raid/raid_info.py
+++ meowth/exts/raid/raid_info.py
@@ -8,11 +8,11 @@ raid_times = {
 }
 
 raid_lists = {
-    1: ["SHUPPET", "DUSKULL", "SNORUNT", "SHINX"],
-    2: ["MISDREAVUS", "SNEASEL", "KIRLIA", "SABLEYE", "MAWILE"],
-    3: ["MACHAMP", "GENGAR", "GRANBULL", "SHARPEDO"],
-    4: ["MAROWAK_ALOLA_FORM", "TOGETIC", "HOUNDOOM", "TYRANITAR", "ABSOL"],
-    5: ["GIRATINA"],
-    6: [],
+    '1': ["SHUPPET", "DUSKULL", "SNORUNT", "SHINX"],
+    '2': ["MISDREAVUS", "SNEASEL", "KIRLIA", "SABLEYE", "MAWILE"],
+    '3': ["MACHAMP", "GENGAR", "GRANBULL", "SHARPEDO"],
+    '4': ["MAROWAK_ALOLA_FORM", "TOGETIC", "HOUNDOOM", "TYRANITAR", "ABSOL"],
+    '5': ["GIRATINA"],
+    '6': [],
     "EX": ["DEOXYS"]
 }

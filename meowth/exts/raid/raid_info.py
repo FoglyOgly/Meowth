5c6a488fce9785bf114c56e352ab5a066b091374
FoglyOgly <lexyk.xiv@gmail.com>
2018-11-15 20:03:49 -0600

    egg_images

--- meowth/exts/raid/raid_info.py
+++ meowth/exts/raid/raid_info.py
@@ -4,7 +4,18 @@ raid_times = {
     '3': (60, 45),
     '4': (60, 45),
     '5': (60, 45),
+    '6': (60, 45),
     "EX": (None, 45)
+}
+
+egg_images = {
+    '1': 'https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/ic_raid_egg_normal.png',
+    '2': 'https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/ic_raid_egg_normal.png',
+    '3': 'https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/ic_raid_egg_rare.png',
+    '4': 'https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/ic_raid_egg_rare.png',
+    '5': 'https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/ic_raid_egg_legendary.png',
+    '6': 'https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/ic_raid_egg_legendary.png'
+
 }
 
 raid_lists = {

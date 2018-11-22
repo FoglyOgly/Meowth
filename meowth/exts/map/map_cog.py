12e3f8cb108c095e459c465e116f2604595ba449
FoglyOgly <lexyk.xiv@gmail.com>
2018-11-22 17:53:24 -0600

    misplaced paren

--- meowth/exts/map/map_cog.py
+++ meowth/exts/map/map_cog.py
@@ -260,7 +260,7 @@ class Mapper(Cog):
         bot = self.bot
         gyms_table = bot.dbi.table('gyms')
         insert = gyms_table.insert()
-        reader = csv.DictReader(codecs.iterdecode(file.readlines()), 'utf-8')
+        reader = csv.DictReader(codecs.iterdecode(file.readlines(), 'utf-8'))
         rows = []
         for row in reader:
             valid_data = {}

b44f590cdad1624556a336e5694f5bd6e49ce9b8
FoglyOgly <kogle@pearidgek12.com>
2019-02-15 10:45:22 -0600

    fix import

--- meowth/exts/map/__init__.py
+++ meowth/exts/map/__init__.py
@@ -1,4 +1,4 @@
-from .map_cog import Mapper, S2_L10, Gym, ReportChannel, PartialPOI
+from .map_cog import Mapper, S2_L10, Gym, ReportChannel, PartialPOI, POI
 
 def setup(bot):
     bot.add_cog(Mapper(bot))

cb3c929a7602808b3b2ade96bad5c5b4aa502555
FoglyOgly <kogle@pearidgek12.com>
2019-03-12 15:00:30 -0500

    change wording

--- meowth/exts/raid/raid_cog.py
+++ meowth/exts/raid/raid_cog.py
@@ -1404,11 +1404,11 @@ class Raid():
     
     async def summary_str(self):
         if self.status == 'egg':
-            pre_str = f'**Level {self.level} Raid:**'
+            pre_str = f'**Level {self.level} Raid at'
         elif self.status == 'hatched':
-            pre_str = f'**Level {self.level} Raid:**'
+            pre_str = f'**Level {self.level} Raid at'
         elif self.status == 'active':
-            pre_str = f'**{await self.pkmn.name()} Raid:**'
+            pre_str = f'**{await self.pkmn.name()} Raid at'
         else:
             return None
         if isinstance(self.gym, Gym):
@@ -1416,7 +1416,7 @@ class Raid():
         else:
             gym_str = self.gym._name
         status_str = self.status_str
-        summary_str = f'🔹 {pre_str} {gym_str}\nRSVPs: {status_str}'
+        summary_str = f'🔹 {pre_str} {gym_str}**\nRSVPs: {status_str}'
         if self.channel_ids:
             channel = self.bot.get_channel(int(self.channel_ids[0]))
             channel_str = channel.mention

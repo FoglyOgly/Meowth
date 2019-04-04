a5ea4326c8fb05bc64d25abd6ca2d64bd2dcf97d
FoglyOgly <kogle@pearidgek12.com>
2019-04-04 10:19:13 -0500

    fix?

--- meowth/__main__.py
+++ meowth/__main__.py
@@ -92,7 +92,7 @@ def main():
     args = parse_cli_args()
     func = partial(run_bot, debug=args.debug, launcher=args.launcher, 
         from_restart=args.fromrestart, shard_count=cores)
-    with Pool(processes=cores) as p:
+    with Pool(cores) as p:
         p.imap_unordered(func, range(cores))
 
 if __name__ == '__main__':

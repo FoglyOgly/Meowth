deff82378f6de75eb0f297b77c176fc61f7fdd64
FoglyOgly <kogle@pearidgek12.com>
2019-04-05 15:06:19 -0500

    fix?

--- meowth/exts/pkmn/errors.py
+++ meowth/exts/pkmn/errors.py
@@ -18,13 +18,12 @@ class MoveInvalid(CommandError):
     'Exception raised, move not learned by Pokemon'
 
     def __init__(self, pokemon, move):
+        self.pokemon = pokemon
+        self.move = move
         if move == pokemon.quickMoveid:
             pokemon.quickMoveid = None
         elif move == pokemon.chargeMoveid:
             pokemon.chargeMoveid = None
         elif move == pokemon.chargeMove2id:
             pokemon.chargeMove2id = None
-        self.pokemon = pokemon
-        self.move = move
-
 

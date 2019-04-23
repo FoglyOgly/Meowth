ecebebab54d9746b7f518babe416a04c64c93dd1
FoglyOgly <kogle@pearidgek12.com>
2019-04-23 15:18:13 -0500

    fix

--- meowth/exts/admin/admin_cog.py
+++ meowth/exts/admin/admin_cog.py
@@ -223,8 +223,8 @@ class AdminCog(Cog):
         if 'meetup' in enabled_commands:
             column = 'category_meetup'
             content = ('How do you want Meetup channels created from this channel '
-            'to be categorized? You can type the name or ID of the category you want 
-            'the channel to appear in.')
+                'to be categorized? You can type the name or ID of the category you want 
+                'the channel to appear in.')
             await ctx.send(content)
             def check(m):
                 return m.author == ctx.message.author and m.channel == ctx.channel

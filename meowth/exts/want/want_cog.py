1cff9e132055fd737be59350cc37df0800c34ea9
FoglyOgly <kogle@pearidgek12.com>
2019-04-26 12:14:39 -0500

    fix?

--- meowth/exts/want/want_cog.py
+++ meowth/exts/want/want_cog.py
@@ -197,6 +197,5 @@ class WantCog(Cog):
         query.where(guild=ctx.guild.id)
         query.where(table['users'].contains_(user_id))
         want_list = await query.get_values()
-        await ctx.send(f'Current want list for {ctx.author.display_name}:\n\n'
-            f"{'\n'.join(want_list)}")
+        await ctx.send(f'Current want list for {ctx.author.display_name}:\n\n{"\n".join(want_list)}')
 

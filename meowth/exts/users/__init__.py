775cf7b6816a606524ac4271d9b49b2a78dc28fe
FoglyOgly <kogle@pearidgek12.com>
2019-04-26 09:54:41 -0500

    fix

--- meowth/exts/users/__init__.py
+++ meowth/exts/users/__init__.py
@@ -1,6 +1,6 @@
 """This cog contains features relating to Pokemon Go user management."""
 
-from .users_cog import Users, MeowthUser
+from .users_cog import Users, MeowthUser, Team
 from . import users_checks
 
 def setup(bot):

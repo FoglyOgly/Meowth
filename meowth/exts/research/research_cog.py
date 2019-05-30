6e89a51fa5ebf0be5db11c48ea78136f61fcce49
FoglyOgly <lexyk.xiv@gmail.com>
2019-05-30 15:38:07 -0500

    fix

--- meowth/exts/research/research_cog.py
+++ meowth/exts/research/research_cog.py
@@ -151,7 +151,7 @@ class Research:
         if reward.startswith('partial'):
             return wants
         elif '/' in reward:
-            reward = ItemReward(selffi.bot, reward)
+            reward = ItemReward(self.bot, reward)
             item = reward.item
             wants.append(item.id)
         elif reward == 'unknown_encounter':

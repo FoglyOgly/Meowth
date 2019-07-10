154f7bbc9c97190f21d7ee46e8e29ba76b67eb3a
Baviaan <baviaannl@hotmail.com>
2019-07-10 02:32:46 +0100

    allow 1min leeway for group.

--- meowth/exts/raid/raid_cog.py
+++ meowth/exts/raid/raid_cog.py
@@ -906,7 +906,7 @@ class RaidCog(Cog):
             stamp = await converter.convert(ctx, group_time)
         if stamp > raid.end:
             raise InvalidTime
-        elif raid.hatch and stamp < raid.hatch - 1:
+        elif raid.hatch and stamp < raid.hatch - 60:
             raise InvalidTime
         grp_id = next(snowflake.create())
         d = {

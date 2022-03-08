a261cb5714970c80e6f558172a95d7ba7daa606e
FoglyOgly <lexyk.xiv@gmail.com>
2022-03-08 11:25:24 -0600

    fix?

--- meowth/exts/raid/raid_commands.py
+++ meowth/exts/raid/raid_commands.py
@@ -6,6 +6,7 @@ class RaidCommands(app_commands.Group):
     @app_commands.command(name='raid')
     @app_commands.guilds(discord.Object(id=344960572649111552))
     async def raid_slash_command(self, interaction: discord.Interaction, boss: str, gym: str, minutes_remaining: app_commands.Range[int, 1, 45]=45):
+        await interaction.response.send_message('Thanks for your report!')
         bot = interaction.client
         raid_cog = bot.get_cog('RaidCog')
         return await raid_cog.raid_slash_command(interaction, boss, gym, minutes_remaining)

f3ceba54910b3ce18cd49b0e7fe386091ee1af12
FoglyOgly <lexyk.xiv@gmail.com>
2022-03-10 21:14:56 -0600

    fix

--- meowth/core/bot.py
+++ meowth/core/bot.py
@@ -157,11 +157,11 @@ class Bot(commands.AutoShardedBot):
 
     @property
     def avatar(self):
-        return self.user.avatar_url_as(static_format='png')
+        return self.user.avatar.url
 
     @property
     def avatar_small(self):
-        return self.user.avatar_url_as(static_format='png', size=64)
+        return self.user.avatar.with_size(64)
 
     @property
     def uptime(self):

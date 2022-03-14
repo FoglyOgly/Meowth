ee36bcd108aac700d59792b4e0f466ef9aa9226f
FoglyOgly <lexyk.xiv@gmail.com>
2022-03-14 17:55:17 -0500

    sync

--- meowth/exts/wild/__init__.py
+++ meowth/exts/wild/__init__.py
@@ -1,4 +1,11 @@
+import discord
+from discord import app_commands
+
 from .wild_cog import WildCog
+from .wild_commands import WildCommands
 
 def setup(bot):
     bot.add_cog(WildCog(bot))
+    tree = app_commands.CommandTree(bot)
+    tree.add_command(WildCommands())
+    bot.loop.create_task(tree.sync())

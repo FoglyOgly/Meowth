77abcdf7aa9768a104f8fdbf34f05e3c14aa9b09
FoglyOgly <lexyk.xiv@gmail.com>
2022-03-16 14:13:26 -0500

    fix

--- meowth/exts/raid/__init__.py
+++ meowth/exts/raid/__init__.py
@@ -6,7 +6,7 @@ from .raid_commands import RaidCommands
 
 def setup(bot):
     bot.add_cog(RaidCog(bot))
-    tree = bot.tree or app_commands.CommandTree(bot)
+    tree = getattr(bot, 'tree', None) or app_commands.CommandTree(bot)
     tree.add_command(RaidCommands())
     bot.loop.create_task(tree.sync())
     bot.tree = tree
--- meowth/exts/wild/__init__.py
+++ meowth/exts/wild/__init__.py
@@ -6,7 +6,7 @@ from .wild_commands import WildCommands
 
 def setup(bot):
     bot.add_cog(WildCog(bot))
-    tree = bot.tree or app_commands.CommandTree(bot)
+    tree = getattr(bot, 'tree', None) or app_commands.CommandTree(bot)
     tree.add_command(WildCommands())
     bot.loop.create_task(tree.sync())
     bot.tree = tree
